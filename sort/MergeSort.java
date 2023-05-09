9420e92504780c918a6586621bc791d2851e681f
Blushyes <2671431017@qq.com>
2023-05-10 01:04:03 +0800

    抽象测试的重复代码

--- sort/MergeSort.java
+++ sort/MergeSort.java
@@ -1,16 +1,16 @@
 import java.util.Arrays;
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 /**
  * 归并排序递归版
  */
 public class MergeSort {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         // int[] nums = {2, 1, 3, 2, 2, 6, 5, 7};
         int[] nums = generateRandomArray(100000000, 100);   // 随机生成一亿个数
-        System.out.println("mergeSort: " + mergeSortDemo(nums) + "ms"); // 一个线程排序
-        System.out.println("concurrentMergeSort: " + concurrentMergeSortDemo(nums) + "ms"); // 两个线程排序
-        System.out.println("customConcurrentMergeSortDemo: " + customConcurrentMergeSortDemo(nums, 6) + "ms");  // 六个线程排序
+        System.out.println("mergeSort: " + SortDemo.test(nums, (tmp) -> mergeSort(tmp, 0, tmp.length - 1)) + "ms"); // 一个线程排序
+        System.out.println("concurrentMergeSort: " + SortDemo.test(nums, (tmp) -> concurrentMergeSort(tmp, 0, tmp.length - 1)) + "ms"); // 两个线程排序
+        System.out.println("customConcurrentMergeSort: " + SortDemo.test(nums, (tmp) -> customConcurrentMergeSort(tmp, 0, tmp.length - 1, 6)) + "ms"); // 六个线程排序
     }
 
     // 归并排序
@@ -41,7 +41,7 @@ public class MergeSort {
     }
 
     // 多线程归并排序
-    private static void concurrentMergeSort(int[] nums, final int L, final int R) throws InterruptedException {
+    private static void concurrentMergeSort(int[] nums, final int L, final int R) {
         if (L == R) {   // 递归基，如果 L==R 说明区间只有1个数，则直接返回，不需要进行排序
             return;
         }
@@ -50,14 +50,17 @@ public class MergeSort {
         Thread rightSort = new Thread(() -> mergeSort(nums, M + 1, R)); // 排序右半段
         leftSort.start();
         rightSort.start();
-        leftSort.join();
-        rightSort.join();
+        try {
+            leftSort.join();
+            rightSort.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         merge(nums, L, M, R);   // 合并两个已经排序好的有序数组
-
     }
 
     // 自定义线程数归并排序
-    private static void customConcurrentMergeSort(int[] nums, final int L, final int R, int threadNumber) throws InterruptedException {
+    private static void customConcurrentMergeSort(int[] nums, final int L, final int R, int threadNumber) {
         if (L == R) {   // 递归基，如果 L==R 说明区间只有1个数，则直接返回，不需要进行排序
             return;
         }
@@ -66,56 +69,25 @@ public class MergeSort {
             if (threadNumber / 2 == 1) {
                 mergeSort(nums, L, M);
             } else {
-                try {
-                    customConcurrentMergeSort(nums, L, M, threadNumber - 1);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+                customConcurrentMergeSort(nums, L, M, threadNumber - 1);
             }
         }); // 排序左半段
         Thread rightSort = new Thread(() -> {
             if (threadNumber / 2 == 1) {
                 mergeSort(nums, M + 1, R);
             } else {
-                try {
-                    customConcurrentMergeSort(nums, M + 1, R, threadNumber - 1);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+                customConcurrentMergeSort(nums, M + 1, R, threadNumber - 1);
             }
         }); // 排序右半段
         leftSort.start();
         rightSort.start();
-        leftSort.join();
-        rightSort.join();
+        try {
+            leftSort.join();
+            rightSort.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         merge(nums, L, M, R);   // 合并两个已经排序好的有序数组
-    }
-
-    private static long mergeSortDemo(int[] nums) {
-        long start = System.currentTimeMillis();
-        int[] tmp = Arrays.copyOf(nums, nums.length);
-        mergeSort(tmp, 0, tmp.length - 1);
-//        System.out.println(Arrays.toString(tmp));
-        long end = System.currentTimeMillis();
-        return end - start;
-    }
-
-    private static long concurrentMergeSortDemo(int[] nums) throws InterruptedException {
-        long start = System.currentTimeMillis();
-        int[] tmp = Arrays.copyOf(nums, nums.length);
-        concurrentMergeSort(tmp, 0, tmp.length - 1);
-//        System.out.println(Arrays.toString(tmp));
-        long end = System.currentTimeMillis();
-        return end - start;
-    }
-
-    private static long customConcurrentMergeSortDemo(int[] nums, final int threadNumber) throws InterruptedException {
-        long start = System.currentTimeMillis();
-        int[] tmp = Arrays.copyOf(nums, nums.length);
-        customConcurrentMergeSort(tmp, 0, tmp.length - 1, threadNumber);
-//        System.out.println(Arrays.toString(tmp));
-        long end = System.currentTimeMillis();
-        return end - start;
     }
 
     public static int[] generateRandomArray(int len, int max){
@@ -128,10 +100,10 @@ public class MergeSort {
 }
 
 class SortDemo {
-    public static long test(int[] nums, boolean outputFlag, Function<int[], Void> sort) {
+    public static long test(int[] nums, boolean outputFlag, Consumer<int[]> sort) {
         long start = System.currentTimeMillis();
         int[] tmp = Arrays.copyOf(nums, nums.length);
-        sort.apply(tmp);
+        sort.accept(tmp);
         if (outputFlag) {
             System.out.println(Arrays.toString(tmp));
         }
@@ -140,10 +112,10 @@ class SortDemo {
 
     }
 
-    public static long test(int[] nums, Function<int[], Void> sort) {
+    public static long test(int[] nums, Consumer<int[]> sort) {
         long start = System.currentTimeMillis();
         int[] tmp = Arrays.copyOf(nums, nums.length);
-        sort.apply(tmp);
+        sort.accept(tmp);
         long end = System.currentTimeMillis();
         return end - start;
     }
