04424aa110205431f145059a4b7e920438428711
Blushyes <2671431017@qq.com>
2023-05-10 22:50:23 +0800

    新增了迭代法

--- sort/MergeSort.java
+++ sort/MergeSort.java
@@ -9,13 +9,14 @@ public class MergeSort {
         // int[] nums = {2, 1, 3, 2, 2, 6, 5, 7};
         int[] nums = SortDemo.generateRandomArray(100000000, 100);   // 随机生成一亿个数
         System.out.println("mergeSort: " + SortDemo.test(nums, (tmp) -> mergeSort(tmp, 0, tmp.length - 1)) + "ms"); // 一个线程排序
+        System.out.println("mergeSortIteration: " + SortDemo.test(nums, MergeSort::mergeSortIteration) + "ms"); // 一个线程排序迭代法
         System.out.println("concurrentMergeSort: " + SortDemo.test(nums, (tmp) -> concurrentMergeSort(tmp, 0, tmp.length - 1)) + "ms"); // 两个线程排序
         System.out.println("customConcurrentMergeSort: " + SortDemo.test(nums, (tmp) -> customConcurrentMergeSort(tmp, 0, tmp.length - 1, 6)) + "ms"); // 六个线程排序
     }
 
     // 归并排序
     private static void mergeSort(int[] nums, final int L, final int R) {
-        if (L == R) {   // 递归基，如果 L==R 说明区间只有1个数，则直接返回，不需要进行排序
+        if (L == R) {   // 递归基，如果 L == R 说明区间只有1个数，则直接返回，不需要进行排序
             return;
         }
         final int M = L + (R - L) / 2;  // 从中间划分
@@ -25,7 +26,7 @@ public class MergeSort {
     }
 
     // 合并两个有序数组
-    private static void merge(int[] nums, final int L,final int M, final int R) {
+    private static void merge(int[] nums, final int L, final int M, final int R) {
         int[] tmp = new int[R - L + 1]; // R - L + 1 为排序区间长度
         int l = L, r = M + 1, p = 0;
         while (l <= M && r <= R) {
@@ -42,7 +43,7 @@ public class MergeSort {
 
     // 多线程归并排序
     private static void concurrentMergeSort(int[] nums, final int L, final int R) {
-        if (L == R) {   // 递归基，如果 L==R 说明区间只有1个数，则直接返回，不需要进行排序
+        if (L == R) {   // 递归基，如果 L == R 说明区间只有1个数，则直接返回，不需要进行排序
             return;
         }
         final int M = L + (R - L) / 2;  // 从中间划分
@@ -61,7 +62,7 @@ public class MergeSort {
 
     // 自定义线程数归并排序
     private static void customConcurrentMergeSort(int[] nums, final int L, final int R, int threadNumber) {
-        if (L == R) {   // 递归基，如果 L==R 说明区间只有1个数，则直接返回，不需要进行排序
+        if (L == R) {   // 递归基，如果 L == R 说明区间只有1个数，则直接返回，不需要进行排序
             return;
         }
         final int M = L + (R - L) / 2;  // 从中间划分
@@ -88,6 +89,30 @@ public class MergeSort {
             throw new RuntimeException(e);
         }
         merge(nums, L, M, R);   // 合并两个已经排序好的有序数组
+    }
+
+    private static void mergeSortIteration(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        final int N = nums.length;
+        int sortSize = 1;   // 待排序区间一半的大小，即左区间大小或右区间大小
+        while (sortSize < N) {
+            int L = 0;
+            while (L < N) {
+                final int M = L + sortSize - 1;
+                if (M >= N) {   // 如果划分点超出数组范围，则不进行合并
+                    break;
+                }
+                final int R = Math.min(M + sortSize, N - 1); // 若右区间未超出数组范围，则选取右区间，否则选取 N - 1
+                merge(nums, L, M, R);
+                L = R + 1;
+            }
+            if (sortSize > N / 2) {
+                break;
+            }
+            sortSize <<= 1;
+        }
     }
 }
 
